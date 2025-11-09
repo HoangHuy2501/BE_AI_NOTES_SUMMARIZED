a78f848e1e006e6f70b957321ab874bde02c10b9
HoangHuy2501 <huy04.developer@gmail.com>
2025-11-09 10:19:24 +0700

    nodemail

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,42 +1,37 @@
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail", // hoặc dùng SMTP riêng nếu bạn có
+  port: 465,          // SSL
+  secure: true,
+  auth: {
+    user: "nhatnguyenhong45@gmail.com",
+    pass: "qzupxnhcxwhrfazo" // dùng app password nếu là Gmail
+  },
+});
 
 // const transporter = nodemailer.createTransport({
-//   service: "gmail", // hoặc dùng SMTP riêng nếu bạn có
+//   host: "smtp-relay.sendinblue.com",
+//   port: 465,       // SSL/TLS
+//   secure: true,    // true = SSL
 //   auth: {
-//     user: "nhatnguyenhong45@gmail.com",
-//     pass: "qzupxnhcxwhrfazo" // dùng app password nếu là Gmail
+//     user: process.env.SENDINBLUE_EMAIL, // ví dụ: no-reply@yourdomain.com
+//     pass: process.env.SENDINBLUE_KEY
 //   },
 // });
-const SibApiV3Sdk = require('@sendinblue/client');
-const client = new SibApiV3Sdk.TransactionalEmailsApi();
-client.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.SendMail);
-const sendVerificationEmail = async (toEmail, token, username) => {
-  const verificationLink = `https://notesummerize.onrender.com/#/verify?token=${token}`;
 
-  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
-    to: [{ email: toEmail }],
-    sender: { name: 'Hoang Huy', email: 'nhatnguyenhong45@gmail.com' },
-    subject: 'Xác thực đăng ký tài khoản',
-    htmlContent: `<h3>Chào bạn ${username}!</h3>
-                  <p>Vui lòng xác thực tài khoản bằng cách nhấn vào link bên dưới:</p>
-                  <a href="${verificationLink}">Xác thực tài khoản</a>`
-  });
-
-  await client.sendTransacEmail(sendSmtpEmail);
-};
-// const sendVerificationEmail = async (toEmail, token,username) => {
-//       const verificationLink = `https://notesummerize.onrender.com/#/verify?token=${token}`;
-//       
-//       await transporter.sendMail({
-//         from: '"Hoang Huy" <your_email@gmail.com>',
-//         to: toEmail,
-//         subject: 'Xác thực đăng ký tài khoản',
-//         html: `
-//           <h3>Chào mừng bạn đến với ${username}!</h3>
-//           <p>Vui lòng xác thực tài khoản bằng cách nhấn vào link bên dưới:</p>
-//           <a href="${verificationLink}">Xác thực tài khoản</a>
-//         `
-//       });
-//     };
-
-    module.exports = sendVerificationEmail;
+const sendVerificationEmail = async (toEmail, token,username) => {
+      const verificationLink = `https://notesummerize.onrender.com/#/verify?token=${token}`;
+      
+      await transporter.sendMail({
+        from: '"Hoang Huy" <your_email@gmail.com>',
+        to: toEmail,
+        subject: 'Xác thực đăng ký tài khoản',
+        html: `
+          <h3>Chào mừng bạn đến với ${username}!</h3>
+          <p>Vui lòng xác thực tài khoản bằng cách nhấn vào link bên dưới:</p>
+          <a href="${verificationLink}">Xác thực tài khoản</a>
+        `
+      });
+    };
+module.exports = sendVerificationEmail;
